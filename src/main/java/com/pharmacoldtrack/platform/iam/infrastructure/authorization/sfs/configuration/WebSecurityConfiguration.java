9314347c9aa176e7cbf5ef70d9bfab9de7038f3a
elmer-riva <elmeraugustoriva@gmail.com>
2025-12-28 05:08:43 -0500

    feat(iam): add security infrastructure
    - Add HashingService and BCrypt implementation
    - Add TokenService and JWT implementation (BearerTokenService)
    - Configure WebSecurity with BearerAuthorizationRequestFilter
    Setup PasswordEncoder bean definition

---
+++ src/main/java/com/pharmacoldtrack/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,72 @@
+package com.pharmacoldtrack.platform.iam.infrastructure.authorization.sfs.configuration;
+
+import com.pharmacoldtrack.platform.iam.infrastructure.authorization.sfs.pipeline.BearerAuthorizationRequestFilter;
+import com.pharmacoldtrack.platform.iam.infrastructure.hashing.bcrypt.BCryptHashingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+    private final UserDetailsService userDetailsService;
+    private final BearerAuthorizationRequestFilter authorizationRequestFilter;
+    private final BCryptHashingService hashingService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return hashingService.encode(rawPassword);
+            }
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return hashingService.matches(rawPassword, encodedPassword);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/authentication/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/webjars/**").permitAll()
+                        .anyRequest().authenticated());
+
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authorizationRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
